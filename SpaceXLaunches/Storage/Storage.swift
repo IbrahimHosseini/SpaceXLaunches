ba4275d500f76dee4fd95399b7696d573ee8fa51
Ebrahim Hosseini <sehosseini@me.com>
2024-05-06 03:42:58 +0330

    implement the User default storage

--- SpaceXLaunches/Storage/Storage.swift
+++ SpaceXLaunches/Storage/Storage.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+protocol Storage {
+    func save(with id: String)
+    func remove(with id: String)
+    func fetchAll() -> [String]
+    func isExist(by id: String) -> Bool
+    func removeAll()
+}
--- SpaceXLaunches/Storage/UserDefaultsStorage.swift
+++ SpaceXLaunches/Storage/UserDefaultsStorage.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+class UserDefaultStorage: Storage {
+
+    private var keys: [String]
+
+    private let key: String
+
+    private let userDefaults = UserDefaults.standard
+
+    init(key: String) {
+        self.key = key
+        keys = userDefaults.stringArray(forKey: key) ?? []
+    }
+
+    func save(with id: String) {
+        keys.append(id)
+        userDefaults.setValue(keys, forKey: key)
+    }
+
+    func remove(with id: String) {
+        keys = keys.filter { $0 != id }
+        userDefaults.setValue(keys, forKey: key)
+    }
+
+    func fetchAll() -> [String] {
+        keys
+    }
+
+    func isExist(by id: String) -> Bool {
+        keys.contains(id)
+    }
+
+    func removeAll() {
+        userDefaults.removeObject(forKey: key)
+    }
+}
