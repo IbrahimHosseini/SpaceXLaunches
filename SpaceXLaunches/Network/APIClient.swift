ff228f3190d4fbd5fb0c7f04e746162e6af5e081
Ebrahim Hosseini <sehosseini@me.com>
2024-05-23 00:53:41 +0330

    update documents

--- SpaceXLaunches/Network/APIClient.swift
+++ SpaceXLaunches/Network/APIClient.swift
@@ -7,8 +7,16 @@
 
 import Foundation
 
+/// This Swift protocol `APIClient` defines a blueprint for an API client.
 protocol APIClient {
+    /// The `associatedtype EndpointType: APIEndpoint` in the `APIClient` protocol is defining an
+    /// associated type requirement. This means that any type conforming to the `APIClient` protocol
+    /// must provide a concrete type for the associated type `EndpointType`, and that type must also
+    /// conform to the `APIEndpoint` protocol.
     associatedtype EndpointType: APIEndpoint
 
+    /// This `func request<T: Decodable>(_ endpoint: EndpointType) async -> (Result<T, NetworkError>)`
+    /// function in the `APIClient` protocol is a method signature that defines a function for making
+    /// asynchronous network requests.
     func request<T: Decodable>(_ endpoint: EndpointType) async -> (Result<T, NetworkError>)
 }
--- SpaceXLaunches/Network/APIEndpoint.swift
+++ SpaceXLaunches/Network/APIEndpoint.swift
@@ -31,15 +31,30 @@ extension APIEndpoint {
     /// set the base url from environment
     var baseURL: URL { URL(string: Constants.baseURL)! }
 
+    /// The line `var method: HTTPMethod { .get }` in the `APIEndpoint` protocol extension is providing
+    /// a default implementation for the `method` property. It is setting the default HTTP method for
+    /// the API endpoint to be `.get`, which is typically used for retrieving data from the server.
     var method: HTTPMethod { .get }
 
+    /// The `var headers: [String: String]?` property in the `APIEndpoint` protocol extension is
+    /// providing a default implementation for the `headers` property. It is setting default headers for
+    /// the API request. In this case, it is setting the `Accept` and `Content-Type` headers to indicate
+    /// that the API endpoint expects and sends data in JSON format.
     var headers: [String: String]? {
         return ["Accept": "application/json",
                 "Content-Type": "application/json"]
     }
 
+    /// The `var httpBody: [String: Any]? { [:] }` line in the `APIEndpoint` protocol extension is
+    /// providing a default implementation for the `httpBody` property. It is setting the default value
+    /// of `httpBody` to an empty dictionary `[:]`. This means that if a specific API endpoint
+    /// conforming to the `APIEndpoint` protocol does not provide a custom implementation for the
+    /// `httpBody` property, it will default to an empty dictionary when creating the API request.
     var httpBody: [String: Any]? { [:] }
 
+    // This `asURLRequest()` function is part of the `APIEndpoint` protocol extension in Swift. It is a
+    // method that converts the endpoint information into a `URLRequest` object that can be used to
+    // make network requests.
     public func asURLRequest() throws -> URLRequest {
         
         /// set the path and create the complete url
